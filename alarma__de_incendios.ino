e297196b5cbf50e3ea0118e73bb010c6011f1710
abril19 <37549442+abril19@users.noreply.github.com>
2018-03-19 17:16:41 +0100

    Add files via upload
    Programa arduino

---
+++ alarma__de_incendios.ino
@@ -0,0 +1,32 @@
+int valorFlameSensor = 0;
+//int EncenderAlarma = 0;
+void setup() {
+ Serial.begin(9600);
+ pinMode(8,OUTPUT);
+ pinMode(12,OUTPUT);
+ pinMode(13,OUTPUT);
+}
+
+void loop() {
+valorFlameSensor = analogRead(0);
+Serial.println(valorFlameSensor);
+if (valorFlameSensor > 400){
+EncenderAlarma();
+}else{
+ApagarAlarma();
+}
+
+}
+void EncenderAlarma(){
+  tone(2,260);
+  digitalWrite(9,HIGH);
+  digitalWrite(3,LOW);
+  delay(250);
+}
+void ApagarAlarma(){
+  noTone(2);
+  digitalWrite(9, LOW);
+  digitalWrite(3, LOW);
+}
+
+  
